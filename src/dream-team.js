80258d600b48592968fb1695ebc1d0922997ebae
ntsh <106670334+tffl@users.noreply.github.com>
2025-01-04 03:19:57 +0300

    feat: dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let dreamTeam = [];
+  if (Array.isArray(members)) {
+    members.forEach((m) => {
+      if (typeof m === 'string') {
+        dreamTeam.push(m.trim()[0].toUpperCase());
+      }
+    })
+    return dreamTeam.sort().join('');
+  }
+
+  return false;
 }
 
 module.exports = {
